54af142281a3350019ca319e3fb8d5093dc2ec36
wooinnn <merlinyuri5@gmail.com>
2026-08-06 22:00:32 +0800

    update

---
+++ jas.js
@@ -0,0 +1,42 @@
+function showGreg() {
+  var btn = document.getElementById('gregButton');
+  var img = document.getElementById('gregPic');
+  var title = document.getElementById('mainTitle');
+  var text = document.getElementById('mainText');
+  
+  
+  var greg2 = document.getElementById('greg2Img');
+  greg2.style.display = 'none';
+  var maintitle = document.getElementById('mainTitle');
+  maintitle.style.display= 'none';
+  var music = document.getElementById('gregMusic');
+var playPromise = music.play();
+
+if (playPromise !== undefined) {
+  playPromise.then(_ => {
+    
+  }).catch(error => {
+    
+    console.log("Playback blocked or failed");
+  });
+}
+
+
+  
+  btn.classList.remove('orbit-animation');
+
+ 
+  document.body.style.backgroundColor = "#ff0000"; 
+  title.style.color = "#00ff00"; 
+  text.style.color = "#ffff00"; 
+  
+  btn.style.backgroundColor = "#ff6600"; 
+  btn.style.color = "#ff00ff"; 
+  btn.style.fontWeight = "bold";
+
+  
+  img.hidden = false;
+  img.classList.remove('orbit-animation');
+  void img.offsetWidth; 
+  img.classList.add('orbit-animation');
+}
